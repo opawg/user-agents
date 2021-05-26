c4b502878a726d3fccdeffef8562b2a4ac595634
AlexandrePlus <52065845+AlexandrePlus@users.noreply.github.com>
2021-05-26 16:34:00 +0200

    add ids for tracking changes

--- src/tests/python/test_compile_regex.py
+++ src/tests/python/test_compile_regex.py
@@ -20,3 +20,36 @@ def test_compile_regex():
         logging.error("regex %s could not be compiled" % regex)
 
   assert not bool(wrong_regexes)
+
+
+def test_ids():
+  ids = set()
+  no_id_issues = []
+  wrong_id_issues = []
+  duplicate_issues = []
+
+  # track errors
+  for user_agent_item in user_agents:
+    primary_regex = user_agent_item['user_agents'][0]
+    if ("id" not in user_agent_item):
+      no_id_issues.append("no id for \"%s\"..." % primary_regex)
+    else:
+      id = user_agent_item['id']
+      if type(id) != int:
+        wrong_id_issues.append("wrong id for \"%s\" where id=\"%s\" (type int expected, %s found)" % (primary_regex, id, type(id)))
+      elif id in ids:
+        duplicate_issues.append("duplicate id for \"%s\" with id=%s" % (primary_regex, id))
+      else:
+        ids.add(id)
+
+  next_suggested_id = max(ids or [0]) + 1
+  
+  # print errors
+  for issue in no_id_issues + wrong_id_issues + duplicate_issues:
+    logging.error(issue)
+  
+  if no_id_issues or wrong_id_issues or duplicate_issues:
+    logging.error("\nno id issue(s):\t\t%s\nwrong id issue(s):\t%s\nduplicate issue(s):\t%s" % (len(no_id_issues), len(wrong_id_issues), len(duplicate_issues)))
+    logging.error("\nnext suggested id:\t%s" % next_suggested_id)
+    
+  assert not no_id_issues and not wrong_id_issues and not duplicate_issues
